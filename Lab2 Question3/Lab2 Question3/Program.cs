581c62f3f68026ae8b2e347ba11f975740719e80
chamo <137000540+chamo2002@users.noreply.github.com>
2024-04-10 11:12:56 +0530

    Add files via upload

---
+++ Lab2 Question3/Lab2 Question3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2_Question3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TemperatureTracker tracker = new TemperatureTracker();
+            tracker.DailyTemperature();
+            tracker.Report();
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
