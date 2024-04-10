b311755d9add302928fb7114c7dec5b152a8f5a9
chamo <137000540+chamo2002@users.noreply.github.com>
2024-04-10 11:11:50 +0530

    Add files via upload

---
+++ Lab2 Question1/Lab2 Question1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2_Question1
+{
+    internal class Book
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book = new Book();
+            book.Title = "Village by the sea"; // semicolon added here
+            book.Author = "Arita Desai";
+
+            Console.WriteLine("Title: " + book.Title);
+            Console.WriteLine("Author: " + book.Author);
+
+            Console.ReadLine();
+        }
+    }
+
+}
